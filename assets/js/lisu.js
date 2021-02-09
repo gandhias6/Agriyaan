53e75b9a017f5235a99d0fa70152a5abf5c5ccac
gandhias6 <gandhias006@gmail.com>
2021-02-09 16:44:43 +0530

    9/2/2021

---
+++ assets/js/lisu.js
@@ -0,0 +1,19 @@
+//to call signup popup page
+function signup() {
+  document.getElementById("signup").style.display = "block";
+}
+
+//to call login popup page
+function login() {
+  document.getElementById("login").style.display = "block";
+}
+
+//to close signup popup page
+function signupclose() {
+  document.getElementById("signup").style.display = "none";
+}
+
+//to close login popup page
+function loginclose() {
+  document.getElementById("login").style.display = "none";
+}
